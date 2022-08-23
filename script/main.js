4723f82f202d73ca30c0e6020114c4cb48a2aa8e
Alfredo Ariel Diaz <alfredodiazjjy@gmail.com>
2022-08-23 08:34:56 -0300

    first commit

---
+++ script/main.js
@@ -0,0 +1,42 @@
+function onClick (event) {
+    event.preventDefault();
+    
+    const mensaje = {
+      name: document.getElementById('comercio').value,
+      email: document.getElementById('titular').value,
+      message: document.getElementById('celular').value
+    }
+    console.log(mensaje);
+  
+  
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(mensaje),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+      .then((response) => response.json())
+      .then((json) => { 
+          console.log(json);
+          Swal.fire(
+              'Enviado',
+              'Gracias por tu comentario', 
+              'success'
+          );
+          cleanForm();
+          /* redirectUrl(); */
+      })
+      .catch((err) => console.log(err));
+  
+  }
+
+  function cleanForm() {
+      let formulario = document.getElementById('formulario');    
+      formulario.reset();    
+  }
+  function redirectUrl(){
+      window.location.href = "https://google.com";    
+  }
+
+  let boton = document.getElementById("enviar");
+    boton.addEventListener("click", onClick);
+  
